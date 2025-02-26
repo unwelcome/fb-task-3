d348c19bd520cccf29254a813a63dce7051203d4
unwelcome <moyapohtaxd@mail.ru>
2025-02-27 01:10:43 +0300

    small fix

--- frontend/scripts/adminExtension.js
+++ frontend/scripts/adminExtension.js
@@ -2,18 +2,50 @@ document.addEventListener('cardsAdded', function() {
     const cardsHTML = document.querySelectorAll('.card');
 
     for(const card of cardsHTML){
-        const btn = document.createElement('input');
-        btn.setAttribute('type', 'button');
-        btn.setAttribute('value', 'Удалить');
-        btn.classList = 'btn btn-delete';
+        //создаем кнопку обновления карточки
+        const btnUpdate = document.createElement('input');
+        btnUpdate.setAttribute('type', 'button');
+        btnUpdate.setAttribute('value', 'Изменить');
+        btnUpdate.classList = 'btn btn-update';
 
-        btn.addEventListener('click', function() {
+        btnUpdate.addEventListener('click', function() {
+            openCardForm(card.getAttribute("data-id"));
+        });
+
+        //создаем кнопку удаления карточки
+        const btnDelete = document.createElement('input');
+        btnDelete.setAttribute('type', 'button');
+        btnDelete.setAttribute('value', 'Удалить');
+        btnDelete.classList = 'btn btn-delete';
+
+        btnDelete.addEventListener('click', function() {
             deleteCardRequest(card.getAttribute("data-id"));
         });
 
-        card.appendChild(btn);
+        //добавляем кнопки на страницу
+        card.appendChild(btnUpdate);
+        card.appendChild(btnDelete);
     }
 });
+
+const openCardForm = () => {
+    
+}
+
+const updateCardRequest = async (id) => {
+    const response = await fetch(`/card/${id}`, {
+        method: 'PUT'
+    });
+
+    console.log(response);
+
+    if(response.status === 204) console.log(`Card #${id} deleted succesful!`);
+    else console.log(`Something went wrong when delete card #${id}!`);
+
+    //просим обновить карточки
+    const event = new CustomEvent('updateCards');
+    document.dispatchEvent(event);
+}
 
 const deleteCardRequest = async (id) => {
     const response = await fetch(`/card/${id}`, {
